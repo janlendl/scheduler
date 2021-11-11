450e46a66d1ca80b67a7cf1b9a56a5d8a3343a84
Lendl Peralta <janlendl@gmail.com>
2021-11-10 21:59:48 -0700

    Create a separate function to output the correct format for spots left

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,19 @@ export default function DayListItem(props) {
     'day-list__item--full': props.spots === 0
   });
 
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return <h3 className='text--light'>no spots remaining</h3>
+    } else if (props.spots === 1) {
+      return <h3 className='text--light'>{ props.spots } spot remaining</h3>
+    } else {
+      return <h3 className='text--light'>{ props.spots } spots remaining</h3>
+    }
+  }  
   return (
     <li className={ dayClass } onClick={() => props.setDay(props.name)}>
       <h2 className='text--regular'>{ props.name }</h2>
-      <h3 className='text--light'>{ props.spots }</h3>
+      { formatSpots() }
     </li>
   );
 }
