7cb0596f56eab7eed0952547d51482e01a465fb8
Lendl Peralta <janlendl@gmail.com>
2021-11-12 19:15:55 -0700

    Add state to track the days in the main application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
@@ -22,6 +22,8 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState('Monday');
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -33,9 +35,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={"Monday"}
-            setDay={day => console.log(day)}
+            days={ days }
+            day={ day }
+            setDay={ setDay }
           />
         </nav>
         <img
