a28ca923a8dbe185a4b2d7b89fcc48f0f3d6d927
Lendl Peralta <janlendl@gmail.com>
2021-11-13 22:46:55 -0700

    Refactor onClick and call the function instead of calling props.id

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -4,25 +4,18 @@ import classNames from 'classnames';
 
 export default function InterviewerListItem(props) {
 
-  const interviewClass  = classNames('interviewers__item', {
+  const interviewerClass  = classNames('interviewers__item', {
     'interviewers__item--selected': props.selected === true,
   });
 
-
-  const showName = () => {
-    if (props.selected === true) {
-      return props.name;
-    }
-  };
-
   return (
-    <li className={ interviewClass } onClick={() => props.setInterviewer(props.id)}>
+    <li className={ interviewerClass } onClick={ props.setInterviewer }>
       <img
         className='interviewers__item-image'
         src={ props.avatar }
         alt={ props.name }
       />
-      { showName() }
+      { props.selected && props.name }
     </li>
   );
 };
