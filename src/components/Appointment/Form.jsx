f73569de433842a7caf9a80f8ce01b4c66cde64e
Lendl Peralta <janlendl@gmail.com>
2021-11-16 22:48:49 -0700

    Add controlled components and props for student and interview

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -1,31 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InterviewerList from 'components/InterviewerList';
 import Button from 'components/Button';
 
-export default function Form() {
+export default function Form(props) {
+  const [ student, setStudent ] = useState(props.student || '');
+  const [ interviewer, setInterviewer ] = useState(props.interviewer || null);
+    
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-              your code goes here
-            */
-          />
+            value={ student }
+            onChange={(event) => setStudent(event.target.value) }
+            />
         </form>
-        <InterviewerList 
-          /* your code goes here */
-        />
+        <InterviewerList
+          interviewers={ props.interviewers }
+          value= { interviewer }
+          onChange={ setInterviewer }
+          />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger {/* your code goes here */}>Cancel</Button>
-          <Button confirm {/* your code goes here */}>Save</Button>
+          <Button danger onClick={ props.onCancel }>Cancel</Button>
+          <Button confirm onClick={ props.onSave }>Save</Button>
         </section>
       </section>
   </main>  
