2a0e3fb0228c9d359755a3bebf26807cbef2836e
Lendl Peralta <janlendl@gmail.com>
2021-11-16 20:28:00 -0700

    Initial creation of story for Form where it has edit and create

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
 import Status from 'components/Appointment/Status';
 import Error from 'components/Appointment/Error';
+import Form from 'components/Appointment/Form';
 
 
 
@@ -173,5 +174,21 @@ storiesOf('Button', module)
       <Error
         message='Could not delete appointment'
         onClose={ action('onClose') }
+      />
+    ))
+    .add('Create', () => (
+      <Form 
+        interviewers={[]}
+        onSave={ action('onSave') }
+        onCancel={ action('onCancel') }
+      />
+    ))
+    .add('Edit', () => (
+      <Form 
+        student='Lendl Peralta'
+        interviewer={3}
+        interviewers={[]}
+        onSave={ action('onSave') }
+        onCancel={ action('onCancel') }
       />
     ));
