3635bd9bbf502ccd40deca1e50531adb1c46455e
Lendl Peralta <janlendl@gmail.com>
2021-11-16 19:30:19 -0700

    Refactor, for now remove className import

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,8 +1,6 @@
   import React from 'react';
   import './styles.scss'
-  import classNames from 'classnames';
-
-
+ 
   export default function Appointment(props) {
     return (
       <article className='appointments'>
