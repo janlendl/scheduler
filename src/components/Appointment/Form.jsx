68ca5d06447cd1ce019905fc8be8f62a313593ab
Lendl Peralta <janlendl@gmail.com>
2021-11-17 18:56:36 -0700

    Create a reset and cancel function for the cancel button

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -5,11 +5,21 @@ import Button from 'components/Button';
 export default function Form(props) {
   const [ student, setStudent ] = useState(props.student || '');
   const [ interviewer, setInterviewer ] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent('');
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
     
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             type="text"
@@ -26,10 +36,10 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={ props.onCancel }>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={ props.onSave }>Save</Button>
         </section>
       </section>
   </main>  
-  )
-}
+  );
+};
