653aa2762a7ecb3504c079fc11442ef880d51880
Lendl Peralta <janlendl@gmail.com>
2021-11-17 18:57:51 -0700

    Import needed components. Set the show component to show student and interviewer

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,11 +1,19 @@
   import React from 'react';
+  import Header from './Header';
+  import Show from './Show';
+  import Empty from './Empty';
+  
   import './styles.scss'
  
   export default function Appointment(props) {
     return (
-      <article className='appointments'>
-        {!props.time && 'No Appointments'}
-        {props.time && `Appointment at ${props.time}` }
+      <article className='appointment'>
+        <Header time={props.time} />
+          {props.interview ? 
+            <Show
+              student={ props.interview.student }
+              interviewer={ props.interview.interviewer} /> 
+            : <Empty />}
       </article>
     )
   }
