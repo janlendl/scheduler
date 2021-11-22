6c3669c7c0218c1e2b033e555e632a3b704784a0
Lendl Peralta <janlendl@gmail.com>
2021-11-21 21:32:29 -0700

    Add getInterview helper

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -18,3 +18,16 @@ export function getAppointmentsForDay(state, day) {
   }
   return appointmentData;
 }
+
+export function getInterview(state, interview) {
+  if (interview !== null) {
+    const interviewID = interview.interviewer;
+    const interviewerData = state.interviewers[interviewID];
+    const interviewerObj = {
+      student: interview.student,
+      interviewer: {...interviewerData }
+    }
+    return interviewerObj;
+  }
+  return null;
+}
