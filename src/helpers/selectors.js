8145967e3e4a1bf57196e17189fe0fa43a9632fe
Lendl Peralta <janlendl@gmail.com>
2021-11-22 23:40:43 -0700

    Create a new function for get interviewers for the day

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -19,6 +19,27 @@ export function getAppointmentsForDay(state, day) {
   return appointmentData;
 }
 
+
+export function getInterviewersForDay(state, day) {
+ 
+  const dayAppointments = [];
+  const interviewersData = [];
+  
+  for (const fromDay of state.days) {
+    if (fromDay.name === day) {
+      dayAppointments.push(...fromDay.appointments);
+    }
+  }
+
+  for (const key in state.interviewers) {
+    const intID = state.interviewers[key].id;
+    if(dayAppointments.includes(intID)) {
+      interviewersData.push(state.interviewers[key]);
+    }
+  }
+  return interviewersData;
+}
+
 export function getInterview(state, interview) {
   if (interview !== null) {
     const interviewID = interview.interviewer;
