1e0382fa919fe51800d706c8cfc3862266760a2d
Lendl Peralta <janlendl@gmail.com>
2021-11-22 22:12:56 -0700

    Use custom hook to render the children components inside application. For now, we added the form component for creating a new appointment

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -2,18 +2,30 @@
   import Header from './Header';
   import Show from './Show';
   import Empty from './Empty';
+  import Form from './Form';
     
-  import './styles.scss'
+  import './styles.scss';
+import useVisualMode from 'hooks/useVisualMode';
+
+  const EMPTY = 'EMPTY';
+  const SHOW = 'SHOW';
+  const CREATE = 'CREATE';
  
   export default function Appointment(props) {
+    const { mode, transition, back } = useVisualMode (
+      props.interview ? SHOW: EMPTY
+    );
     return (
       <article className='appointment'>
         <Header time={props.time} />
-          {props.interview ? 
+          {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+          {mode === CREATE && <Form interviewers={[]} onCancel={() => back(EMPTY)}/>}
+          {mode === SHOW && (
             <Show
-              student={ props.interview.student }
-              interviewer={ props.interview.interviewer } /> 
-            : <Empty />}
+                student={props.interview.student}
+                interviewer={props.interview.interviewer}
+            />
+)}
       </article>
     )
   }
