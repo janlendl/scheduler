90dfe9b2e46429acbea72eb93b4728aa1dae4bf7
Lendl Peralta <janlendl@gmail.com>
2021-11-22 21:33:56 -0700

    Create custom hooks with unit test function

---
+++ src/hooks/useVisualMode.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+    const [mode, setMode] = useState(initial);
+    const [history, setHistory] = useState([initial]);
+    
+  // move to the next component and save the previous state to history
+    const transition = (newMode, replace = false) => {
+      if (replace) {
+        history.pop()
+        setHistory(history);
+      }
+      setHistory((prev) => [...prev, newMode]);
+      setMode(newMode);
+    };
+
+  // go back to the previous component saved
+    const back = () => {
+      if (history.length > 1) {
+        history.pop();
+      }
+      if (history.length > 0) {
+        setMode(history[history.length - 1]);
+      }
+    };
+
+    return { mode, transition, back };
+};
+
