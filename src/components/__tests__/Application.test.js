aabafe58c73a8c66461771e279ce4e11dd30c387
Lendl Peralta <janlendl@gmail.com>
2021-12-05 22:41:00 -0700

    Add test for adding an appointment scenario

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,15 +1,51 @@
-import React from "react";
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
-import Application from "components/Application";
+import React from 'react';
+import Application from 'components/Application';
+import { 
+  render, 
+  cleanup, 
+  waitForElement, 
+  fireEvent, 
+  getByText, 
+  prettyDOM, 
+  getAllByTestId, 
+  getByAltText,
+  getByPlaceholderText 
+} from '@testing-library/react';
 
 afterEach(cleanup);
 
-it("renders defaults to Monday and changes the schedule when a new day is selectedwithout crashing", () => {
-  const { getByText } = render(<Application />);
+describe('Application', () => {
 
-  return waitForElement(() => getByText('Monday')).then(() => {
+  it('changes the schedule when a new day is selected', async () => {
+    const { getByText } = render(<Application />);
+
+    await waitForElement(() => getByText('Monday'));
     fireEvent.click(getByText('Tuesday'));
-
+  
     expect(getByText('Leopold Silvers')).toBeInTheDocument();
+  
   });
-});
+  
+  it('loads data, books an interview and reduces the spots remaining for the first day by 1', async () => {
+    const { container } = render(<Application />);
+    
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+
+    const appointments = getAllByTestId(container, 'appointment');
+    const appointment = appointments[0];
+
+    
+    fireEvent.click(getByAltText(appointment, 'Add'));
+    
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: 'Lydia Miller-Jones' }
+    });
+    
+    fireEvent.click(getByAltText(appointment, 'Sylvia Palmer'));
+    
+    fireEvent.click(getByText(appointment, 'Save'));
+    
+    console.log(prettyDOM(appointment));
+  });
+  
+}) 
