fe912ba237b770e2eae2c89727a6c5a775660993
Lendl Peralta <janlendl@gmail.com>
2021-12-06 00:28:39 -0700

    Add data-id for days

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -19,7 +19,7 @@ export default function DayListItem(props) {
     }
   }  
   return (
-    <li className={ dayClass } onClick={() => props.setDay(props.name)}>
+    <li className={ dayClass } onClick={() => props.setDay(props.name)} data-testid='day'>
       <h2 className='text--regular'>{ props.name }</h2>
       { formatSpots() }
     </li>
