e00f587e569bbe1585bb6bb83c381070963a8768
Lendl Peralta <janlendl@gmail.com>
2021-12-05 22:40:23 -0700

    Add a mock for axios.put request

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -79,5 +79,12 @@ export default {
         data: fixtures.interviewers
       });
     }
+  }),
+
+  put: jest.fn(() => {
+    return Promise.resolve({
+      status: 204,
+      statusTest: 'No Content'
+    });
   })
 };
