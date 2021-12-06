d492af5679dfa0a56e46f09a6e83ace430a5ac5d
Lendl Peralta <janlendl@gmail.com>
2021-12-06 16:09:41 -0700

    Create edit test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -58,4 +58,76 @@ describe('Application', () => {
     
   });
   
-}) 
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    // 1. Render the Application.
+    const { container } = render(<Application />);
+  
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointment = getAllByTestId(container, 'appointment').find(
+      appointment => queryByText(appointment, 'Archie Cohen')
+    );
+
+    fireEvent.click(getByAltText(appointment, 'Delete'));
+
+    expect(getByText(appointment, 'Are you sure you like to delete?')).toBeInTheDocument();
+    
+    // click confirm 
+    fireEvent.click(queryByText(appointment, 'Confirm'));
+    
+    // deleting animation
+    expect(getByText(appointment, 'Deleting...')).toBeInTheDocument();
+    
+    // wait until the day is empty and add button shows up
+    await waitForElement(() => getByAltText(appointment,'Add'));
+
+    // check day
+    const day = getAllByTestId(container, 'day').find(day => 
+      queryByText(day, 'Monday')
+    );
+
+    // spots needs to increment by 1
+    expect(getByText(day, '2 spots remaining')).toBeInTheDocument();
+
+  });
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    // Render the application
+    const { debug, container } = render(<Application />);
+
+    // Wait until Archie Cohen is displayed
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+
+    const appointment = getAllByTestId(container, 'appointment').find(
+      appointment => queryByText(appointment, 'Archie Cohen')
+    );
+
+    // Click the edit button
+    fireEvent.click(getByAltText(appointment, 'Edit'));
+    // Makes the changes by editing the name
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: 'Lendl Peralta' }
+    });
+
+    // Makes the changes by selecting a new interviewer
+    fireEvent.click(getByAltText(appointment, 'Tori Malcolm'));
+    // Save button
+    fireEvent.click(getByText(appointment, 'Save'));
+
+    // Saving mode
+    expect(getByText(appointment, 'Saving...')).toBeInTheDocument();
+
+    //Show updated data
+    await waitForElement(() => getByText(appointment, 'Lendl Peralta'));
+
+    // Mondays spot will remain the same
+    const day = getAllByTestId(container, 'day').find(day =>
+      queryByText(day, 'Monday')
+    );
+
+    expect(getByText(day, '1 spot remaining'));
+
+  })
+  
+})
